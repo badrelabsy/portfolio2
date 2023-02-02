814ea2c14dce01bff5e844b9edec3f45e64e5d64
badrelabsy <119290574+badrelabsy@users.noreply.github.com>
2023-02-02 21:04:26 +0100

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,89 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/meriamtalbi",
+    "name": "badr eddine labsy",
+    "role": "bakend developer",
+    "linkedinId":"badr eddine labsy",
+    "skypeid": "badr eddine labsy",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://github.com/badrelabsy",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/badrelabsy",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/LabsyBadr",
+          "className":"fa fa-twitter"
+        }
+      ],
+      
+      
+    "aboutme":"I am currently a  student at The sup-mti . I am a self taught bakend Web Developer, currently diving deeper into .net",
+    "address":"maroc",
+    "website":"https://github.com/badrelabsy",
+    "education":[
+      {
+        "UniversityName":"Ecole Supérieur de Management, Télécommunication et d'Informatique",
+        "specialization":"dev informatique",
+       
+       
+      },
+      {
+        "UniversityName":"supmti",
+        "specialization":"dev",
+        
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"matu",
+        "specialization":"cluster",
+        
+        
+      },
+      {
+        "CompanyName":"matu",
+        "specialization":"Some specialization",
+       
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":".Net"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/app1.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"user666"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"user-25"
+      }
+    ]
+  }
+  
+  export default resumeData
